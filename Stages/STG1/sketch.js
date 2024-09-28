ed03688300774a5bb2067fabe046a296e602b239
Archie <149274597+MagicArchie@users.noreply.github.com>
2024-09-28 19:18:28 +0300

    Update sketch.js

--- Stages/STG1/sketch.js
+++ Stages/STG1/sketch.js
@@ -333,7 +333,7 @@ function draw() {
   rect(1260, -30, 100, 140, 15);
 
   fill(235, 131, 52, 30);
-  rect(0, 0, 150, canvasHeight);
+  rect(0, 0, 150, height);
 
   if (score == 45) {
     if (!oneUse2) {
